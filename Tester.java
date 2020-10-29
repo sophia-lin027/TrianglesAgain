52dec06fc1ef47372f398f4aca0b610a0e86fa2a
Sophia Lin <sophia@Sophias-Mac.fios-router.home>
2020-10-29 18:57:03 -0400

    added 2.3.a

--- Tester.java
+++ Tester.java
@@ -1,7 +1,9 @@
 public class Tester {
   public static void main (String[] args) {
     Point p1 = new Point(1, 2);
+    Point p2 = new Point (2, 3);
     System.out.println(p1.getX());
     System.out.println(p1.getY());
+    System.out.println(p1.distanceTo(p2));
   }
 }
