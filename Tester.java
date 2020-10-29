f986db02aec6fcee3646488e0705fb2d18cf3578
Sophia Lin <sophia@Sophias-Mac.fios-router.home>
2020-10-29 19:41:14 -0400

    added area 2.4.c

--- Tester.java
+++ Tester.java
@@ -10,5 +10,6 @@ public class Tester {
     System.out.println(p1.distanceTo(p2));
     System.out.println(p1.equals(p3));
     System.out.println(t1.getPerimeter());
+    System.out.println(t1.getArea());
   }
 }
