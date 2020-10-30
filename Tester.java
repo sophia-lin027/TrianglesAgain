088735de6c642669154e148835fb604aa929aab2
Sophia Lin <sophia@Sophias-Mac.fios-router.home>
2020-10-29 21:23:23 -0400

    added classify 2.4.c

--- Tester.java
+++ Tester.java
@@ -4,12 +4,16 @@ public class Tester {
     Point p2 = new Point (7, 5);
     Point p3 = new Point (2, 2);
     Point p4 = new Point (3, 9);
+    Point p5 = new Point (2, 2);
     Triangle t1 = new Triangle (p1, p2, p4);
+    Triangle t2 = new Triangle (p1, p3, p5);
     System.out.println(p1.getX());
     System.out.println(p1.getY());
     System.out.println(p1.distanceTo(p2));
     System.out.println(p1.equals(p3));
     System.out.println(t1.getPerimeter());
     System.out.println(t1.getArea());
+    System.out.println(t1.classify());
+    System.out.println(t2.classify());
   }
 }
