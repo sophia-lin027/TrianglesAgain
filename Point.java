2edcd5879f1c1a95ac72137ac813faf26a4809f1
Sophia Lin <sophia@Sophias-Mac.fios-router.home>
2020-10-29 23:51:13 -0400

    slight change

--- Point.java
+++ Point.java
@@ -19,8 +19,8 @@ public class Point {
     return y;
   }
 
-  public double distanceTo(Point other) {
-    return Math.sqrt((Math.pow((other.getX() - getX()), 2) + Math.pow((other.getY() - getY()), 2)));
+  public double distanceTo(Point other){
+    return Math.sqrt((Math.pow((other.getX()-getX()),2) + Math.pow((other.getY()-getY()),2)));
   }
 
   public boolean equals (Point other) {
